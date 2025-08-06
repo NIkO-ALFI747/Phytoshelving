8df75903076f0289b2714541e1796829954d65a2
Nikolay Ermilov <nikolayermilov774@gmail.com>
2025-08-06 14:55:09 +0500

    Implementation of the Supabase DB connection

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const surveyController = require("./controllers/surveyController");
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_ORIGIN || "http://localhost:3000",
   },
 });
 
